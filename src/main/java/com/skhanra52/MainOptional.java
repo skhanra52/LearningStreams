33206b745e201d222dfc3255c470e8d0ec4beb79
Suman Kalyan Khanra <skhanra52@gmail.com>
2026-01-04 21:48:29 +0530

    Stream with optional types

--- src/main/java/com/skhanra52/MainOptional.java
+++ src/main/java/com/skhanra52/MainOptional.java
@@ -62,29 +62,79 @@ public class MainOptional {
         Optional<Student> o1 = getStudent(null, "first");
         System.out.println("Empty = "+o1.isEmpty()+", Present = "+ o1.isPresent());
         System.out.println("o1 : "+o1);
-        System.out.println("-------------------------------------------");
+//        System.out.println(o1.get()); // produce .NoSuchElementException: No value present
+        System.out.println("-------------o2------------------------------");
 
         Optional<Student> o2 = getStudent(new ArrayList<>(), "first");
         System.out.println("Empty = "+o2.isEmpty()+", Present = "+ o2.isPresent());
         System.out.println("o2 : "+o2);
-        System.out.println("---------------------------------------------");
+//        System.out.println(o2.get()); // produce .NoSuchElementException: No value present
+        o2.ifPresentOrElse(System.out::println, () -> System.out.println("----> Empty"));
+        System.out.println("---------------o3------------------------------");
 
         Optional<Student> o3 = getStudent(students, "first");
         System.out.println("Empty = "+o3.isEmpty()+", Present = "+ o3.isPresent());
         System.out.println("o3 : "+o3);
+//        System.out.println(o3.get()); // compiler warning: 'Optional.get()' without 'isPresent()' check
+        o3.ifPresent(System.out::println);
+        // another version of the code with optional type and ternary operator.
+
+        // Student firstStudent = o2.isPresent() ? o2.get() : null;
+        // or use functional style expression
+        Student firstStudent = o3.orElse(null);
+        long id = (firstStudent == null) ? -1 : firstStudent.getStudentId();
+        System.out.println("First student's id is: "+id);
+
+        // Let say instead of returning null we would like to return some dummy value if o2 is not present
+        // Even though I have o3 value the getDummyStudent(jmc) getting called as I have passed it as argument
+        // to "orElse(getDummyStudent(jmc))" , to avoid this we can use "orElseGet(Supplier<? extends student> supplier)"
+//        Student firstStudentElseDummy = o3.orElse(getDummyStudent(jmc));
+        Student firstStudentElseDummy = o3.orElseGet(() -> getDummyStudent(jmc));
+        long firstStudId = firstStudentElseDummy.getStudentId();
+        System.out.println("First student's id or dummy Student id is: "+id);
+
+        /*
+        Optional class, this class has methods, that seem to mirror some of the stream pipeline operations.
+        set up a little code given below.
+         */
+
+        List<String> countries = students.stream()
+                .map(Student::getCountryCode)
+                .distinct()
+                .toList();
+
+        Optional.of(countries)
+                .map(list -> String.join(",", list))
+                .filter((item) -> item.contains("FR"))
+                .ifPresentOrElse(System.out::println, () -> System.out.println("FR is missing"));
+
+        /*
+        The downside of optional:
+        ----------------------------------------
+        -> Wrapping element in option consumes more memory and has the possibility of showing down execution.
+        -> Wrapping elements in option adds complexity and reduce readability of the code.
+        -> Optional is not serializable.
+        -> Using option for the fields or method parameters are not recommended.
+         */
+
     }
 
     private static Optional<Student> getStudent(List<Student> list, String type){
 
         if(list == null || list.isEmpty()) {
-        //    return null; // if you return null it will throw an error "Cannot invoke "java.util.Optional.isEmpty()"
+        //    return null; // if you return null it will throw an error "can not invoke" java.util.Optional.isEmpty()"
                          // because "o1" is null"
             return Optional.empty();
         } else if(type.equals("first")){
-            return Optional.of(list.get(0));
+            return Optional.ofNullable(list.get(0));
         } else if(type.equals("last")){
-            return Optional.of(list.get(list.size() - 1));
+            return Optional.ofNullable(list.get(list.size() - 1));
         }
-        return Optional.of(list.get(new Random().nextInt(list.size())));
+        return Optional.ofNullable(list.get(new Random().nextInt(list.size())));
+    }
+
+    private static Student getDummyStudent(Course ...course){
+        System.out.println("Getting dummy student");
+        return new Student("No", 1, 1, "No", false, course);
     }
 }
